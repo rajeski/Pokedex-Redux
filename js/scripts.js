d0442c99fdcd9cc515c7d899f9323937fda4ba10
rajeski <asiajsr@gmail.com>
2019-12-03 17:29:51 +0900

    Complete Project Revamp
    1) Edited the style.css file; 2) Renamed the DIST-folder files; 3) Deleted / uploaded new images to the IMG-folder; 4) Edited the index.html file; and, 5) Edited the scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,107 @@
-var pokemonRepository = (function() {
-    /* IIFE (Pokedex-san Array Inside) */
-    'use strict';
-    var repository = [];
-  
-    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-    function addListItem(pokemon) {
-      var pokelist = $(
-        '.list-group'
-      ); /* Place JQuery List Item & Button Tags w/ CSS-Class Styles */
-      var listitem = $('<li class="list-group-item"></li>');
-      $(pokelist).append(listitem);
-      var btn = $(
-        '<button class="btn btn-dark" data-toggle="modal" data-target="#PokeModal"></button>'
-      );
-      $(btn).text(pokemon.name);
-      $(listitem).append(btn);
-      btn.on('click', function() {
-        /* JQuery Click Button, Event Listener for Displaying showDetails Function Properties */
-        showDetails(pokemon);
+var pokemonRepository = (function() {    //* Begin IIFE
+  var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  var $pokemonList = $('ul');
+
+  //* Function for adding new Pokemon data
+  function add(pokemon) {
+    // Needs to be an 'object' type
+    if (typeof pokemon !== 'object') {
+      return 'Not a valid input'
+    }else{
+    repository.push(pokemon);
+    }
+  }
+
+  //* Function for pulling all Pokemon data
+  function getAll() {
+    return repository;
+  }
+
+  //* Function for adding a list for each Pokemon object
+  function addListItem(pokemon) {
+    var $listItem = $('<li></li>');
+    $pokemonList.append($listItem);
+    var $button = $('<button type="button" id="pokemon-button" class="btn btn-outline-light" data-toggle="modal" data-target="#exampleModalCenter">' + pokemon.name + '</button>');
+    $listItem.append($button);
+    $button.on('click', function() {
+      showDetails(pokemon)
+    })
+  }
+
+  //* Function for loading Pokemon list from the API
+  function loadList() {
+    return $.ajax(apiUrl, {dataType: 'json'}).then(function(responseJSON) {
+      return responseJSON;
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
       });
-    }
-    function showDetails(item) {
-      pokemonRepository.loadDetails(item).then(function() {
-        showModal(item);
-      });
-    }
-    function add(name) {
-      /* For Adding Additional Pokemon Attributes to Pokedex-san Array */
-      repository.push(name);
-    }
-  
-    function catchAll() {
-      /* Function to Return Pokedex-san Objects in the Array */
-      return repository;
-    }
-  
-    function loadList() {
-      return $.ajax(apiUrl, { dataType: 'json' })
-        .then(function(item) {
-          /* Replaced Fetch Option w/ Ajax Option */
-  
-          $.each(item.results, function(index, item) {
-            var pokemon = {
-              name: item.name,
-              detailsUrl: item.url
-            };
-            add(pokemon);
-          });
-        })
-        .catch(function(error) {
-          /* Load Functions (in Order) for (Pokemon API) Data Retrieval */
-          document.write(error);
-        });
-    }
-    function loadDetails(item) {
-      var url = item.detailsUrl;
-      return $.ajax(url)
-        .then(function(details) {
-          /* Replaced Fetch Option w/ Ajax Option */
-          item.imageUrl = details.sprites.front_default;
-          item.height = details.height;
-          item.weight = details.weight;
-          item.types = Object.keys(details.types);
-        })
-        .catch(function(error) {
-          document.write(error);
-        });
-    }
-    /* Begin Jquery Model Definition */
-    function showModal(item) {
-      $('#pokeName').text(item.name);
-      $('#pokeImg').attr('src', item.imageUrl);
-      $('#pokeHeight').text('Height: ' + item.height);
-      $('#pokeWeight').text('Weight: ' + item.weight);
-    }
-    /* End Jquery Model Definition */
-    return {
-      /* All Previous Functions Returned (in Order) Available Outside of the IIFE */
-      add: add,
-      catchAll: catchAll,
-      addListItem: addListItem,
-      loadList: loadList,
-      loadDetails: loadDetails
-    };
-  })();
-  
-  pokemonRepository.loadList().then(function() {
-    'use strict';
-    pokemonRepository.catchAll().forEach(function(pokemon) {
-      pokemonRepository.addListItem(pokemon);
+    }).catch(function(e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return $.ajax(url, {dataType: 'json'}).then(function(responseJSON) {
+      return responseJSON;
+    }).then(function(details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.weight = details.weight;
+      item.types = Object.keys(details.types);
+    }).catch(function(e) {
+      console.error(e);
+    })
+  }
+
+  //* Function for showing modal for Pokemon data
+  function showModal(item) {
+    console.log('TCL: showModal -> item', item.imageUrl);
+
+    //* Create element for Pokemon name
+    var $nameElement = $('h5');
+    $nameElement.html(item.name.charAt(0).toUpperCase() + item.name.slice(1));
+
+    var $imageElement = $('<img src="' + item.imageUrl + '">')
+    $('div.pokemon-img').html($imageElement)
+
+    var $heightElement = $('div.pokemon-info');
+    $heightElement.html('Height: ' + item.height);
+  }
+
+  //* Function for showing each Pokemons' details
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function() {
+      console.log(item);
+      return item;
+    }).then(function(item) {
+      console.log('TCL: showDetails -> item', item);
+      showModal(item);
     });
+  }
+
+  return{
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showModal: showModal,
+  };
+})();
+
+
+//* Creates Pokemon list with Pokemon's name on the button
+pokemonRepository.loadList().then(function() {
+  //* All the requisite data is loaded
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
   });
+});
